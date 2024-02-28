34690c5a78bde7ac38d511268b001075fb32ab1c
Chapderlane <154477996+Chapderlane@users.noreply.github.com>
2024-02-28 04:45:21 +0000

    Third Commit

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Jumbotron = () => {
-    return  <div class="p-5 mb-4 bg-body-tertiary rounded-3">
-    <div class="container-fluid py-5">
-      <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-      <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-      <button class="btn btn-primary btn-lg" type="button">Example button</button>
-    </div>
-  </div>
+    return (
+        <div className="p-5 mb-4 rounded-3" style={{ backgroundColor: "light gray" }}>
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+                <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <button className="btn btn-primary btn-lg" type="button">Call to Action!</button>
+            </div>
+        </div>
+    );
 }
 
-export default Jumbotron
+export default Jumbotron;
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 
 const Navbar = () => {
-    return  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-          <a class="nav-link" href="#">About</a>
-          <a class="nav-link" href="#">Services</a>
-          <a class="nav-link" href="#">Contact</a>
-        </div>
-      </div>
-    </div>
-  </nav>
-}
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    Start Bootstrap
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#">
+                            Home
+                        </a>
+                        <a className="nav-link" href="#">
+                            About
+                        </a>
+                        <a className="nav-link" href="#">
+                            Services
+                        </a>
+                        <a className="nav-link" href="#">
+                            Contact
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
